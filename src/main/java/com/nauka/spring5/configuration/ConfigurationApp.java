03c7ac14247b46e908c7dc868eb657afcd748cd3
Igor Szczerbicki <pl4cek@outlook.com>
2018-05-09 09:27:08 +0200

    @Configuration
    SportConfig
    - plik zawierajacy konfiguracje dla Spring'a
    - adnotacja @ComponentScan wlacza skanowanie w poszukiwaniu klas z adnotacja @Component
    
    ConfigurationApp
    - pobieranie konfiguracji z klasy Java

--- src/main/java/com/nauka/spring5/configuration/ConfigurationApp.java
+++ src/main/java/com/nauka/spring5/configuration/ConfigurationApp.java
@@ -1,12 +1,21 @@
 package com.nauka.spring5.configuration;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ConfigurationApp {
 
 	public static void main(String[] args) {
-		//zaladowanie pliku konfiguracji
+		//wczytanie klasy konfiguracji
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 				
 		//zaladowanie z kontenera
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		//wywolanie metod na Beanie
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getFortune());
 		
 		//zamkniecie kotekstu
+		context.close();
 	}
 }
---
+++ src/main/java/com/nauka/spring5/configuration/SportConfig.java
@@ -0,0 +1,9 @@
+package com.nauka.spring5.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nauka.spring5.configuration")
+public class SportConfig {
+}
